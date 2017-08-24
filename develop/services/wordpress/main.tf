f4d6238a2d7b2615580f126b6a50c83138470f4f
Ray Walker <hello@raywalker.it>
2017-08-24 16:03:34 +1000

    use github module source

--- develop/services/wordpress/main.tf
+++ develop/services/wordpress/main.tf
@@ -6,43 +6,31 @@ variable "cluster_master_username" {}
 variable "cluster_master_password" {}
 
 module "cloudsql" {
-  # source = "git::https://github.com/27Bslash6/terraform-gcp-cloudsql.git?ref=master"
-  source = "../../../../terraform-gcp-cloudsql"
+  source = "git::https://github.com/27Bslash6/terraform-gcp-cloudsql.git?ref=master"
+
+  # source = "../../../../terraform-gcp-cloudsql"
 
   cloudsql_master_name = "${var.sql_instance_name}"
-
   cloudsql_username = "wordpress"
   cloudsql_userpass = "thisisnotapassword"
 }
 
-#
-# output "cloudsql_master_ip" {
-#   value = "${module.cloudsql.module.google_sql_database_instance.master.ip}"
-# }
-
 module "redis" {
-  # source = "git::https://github.com/27Bslash6/terraform-gke.git?ref=0.0.3"
-  source = "../../../../terraform-gke"
+  source = "git::https://github.com/27Bslash6/terraform-gke.git?ref=master"
 
-  cluster_name = "${var.redis_cluster_name}"
+  # source = "../../../../terraform-gke"
 
-  initial_node_count = 1
-  machine_type       = "f1-micro"
-  disk_size_gb       = "10"
-
+  cluster_name        = "${var.redis_cluster_name}"
+  initial_node_count  = 1
+  machine_type        = "f1-micro"
+  disk_size_gb        = "10"
   gcp_master_username = "${var.cluster_master_username}"
   gcp_master_password = "${var.cluster_master_password}"
-
-  gcp_primary_zone = "${var.gcp_region}-a"
-
+  gcp_primary_zone    = "${var.gcp_region}-a"
   gcp_additional_zones = [
     "${var.gcp_region}-b",
     "${var.gcp_region}-c",
   ]
-
-  # Install Helm provider:
-  # https://github.com/mcuadros/terraform-provider-helm/blob/master/docs/index.html.md
-
   custom_provisioner = <<EOF
 helm init && \
 sleep 10 && \
@@ -53,25 +41,22 @@ EOF
 }
 
 module "wordpress" {
-  # source = "git::https://github.com/27Bslash6/terraform-gke.git?ref=0.0.3"
-  source = "../../../../terraform-gke"
+  source = "git::https://github.com/27Bslash6/terraform-gke.git?ref=master"
 
-  cluster_name = "${var.wordpress_cluster_name}"
+  # source = "../../../../terraform-gke"
 
-  initial_node_count = 1
-  machine_type       = "g1-small"
-  disk_size_gb       = "10"
-
+  cluster_name        = "${var.wordpress_cluster_name}"
+  initial_node_count  = 1
+  machine_type        = "g1-small"
+  disk_size_gb        = "10"
   gcp_master_username = "${var.cluster_master_username}"
   gcp_master_password = "${var.cluster_master_password}"
-
-  gcp_primary_zone = "${var.gcp_region}-a"
-
+  gcp_primary_zone    = "${var.gcp_region}-a"
   gcp_additional_zones = [
     "${var.gcp_region}-b",
   ]
 
-  # Install Helm provider:
+  # @todo Install Helm provider:
   # https://github.com/mcuadros/terraform-provider-helm/blob/master/docs/index.html.md
 
   custom_provisioner = <<EOF
